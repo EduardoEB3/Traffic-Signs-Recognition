f0b5bf4e846f6dea62d1db14650c8470be665904
EduardoEB3 <alu0101230382@ull.edu.es>
2022-03-22 21:23:12 +0000

    Update App

--- Development/app/app.py
+++ Development/app/app.py
@@ -5,6 +5,12 @@ import pandas as pd
 import numpy as np
 import time
 from PIL import Image
+from keras.models import load_model
+from signs import *
+import markdown, codecs
+
+# Load the trained model to classify sign
+model = load_model("../model/traffic_classifier.h5")
 
 st.title("Traffic Signs Recognition")
 
@@ -13,22 +19,45 @@ st.sidebar.title("Menu")
 
 option = st.sidebar.selectbox(
     "Select the options",
-    ("Select", "Upload an image", "Settings"),
+    ("Select", "Upload an image", "Information"),
 )
 
 if option != "Select":
     if option == "Upload an image":
         uploaded_file = st.sidebar.file_uploader("Choose a file", type=["jpg", "png"])
 
-    image = Image.new("RGB", (1, 1))
-    read = False
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        read = True
-
-    if read:
-        st.image(image.resize((2 * image.width, 2 * image.height)))
+        image = Image.new("RGB", (1, 1))
         read = False
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            read = True
+
+        if read:
+            image = image.resize((30, 30))
+            st.image(image.resize((10 * image.width, 10 * image.height)))
+            read = False
+            if st.button("Classify Image"):
+                image = np.expand_dims(image, axis=0)
+                image = np.array(image)
+                print(image.shape)
+                pred = model.predict([image])[0]
+                newPred = pred + 1
+                newPred = [int(i) for i in newPred]
+                maxValue = max(newPred)
+                sign = classes[newPred.index(maxValue) + 1]
+                print(sign)
+                loadingBar = st.progress(0)
+                for percentComplete in range(100):
+                    time.sleep(0.01)
+                    loadingBar.progress(percentComplete + 1)
+                st.caption(sign)
+
+    elif option == "Information":
+        inputFile = codecs.open(
+            "../information/information.md", mode="r", encoding="utf-8"
+        )
+        text = inputFile.read()
+        st.markdown(markdown.markdown(text), unsafe_allow_html=True)
 
 else:
     image = Image.open("../img/ULL.png")
@@ -38,4 +67,4 @@ else:
     image = image.resize((454, 454))
     st.image(image)
     st.caption("Eduardo Expósito Barrera")
-    st.caption("Univerrsidad de La Laguna")
+    st.caption("Universidad de La Laguna")
---
+++ Development/information/information.md
@@ -0,0 +1,8 @@
+### Hi there 👋
+- 📖  I am a 4th year student of Computer Engineering at the University of La Laguna, specifically the computing branch.
+
+## Contact with me 🙋🏽‍♂️
+<p align="center">
+<a href="https://www.linkedin.com/in/eduardo-exp%C3%B3sito-barrera-b31b62234/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" height="30" width="40" /></a>
+<a href="https://github.com/EduardoEB3" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg" height="30" width="40" /></a>
+</p>
