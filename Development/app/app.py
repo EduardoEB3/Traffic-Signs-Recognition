09ca219383bacbcb248fe27682779700fe2e441f
EduardoEB3 <alu0101230382@ull.edu.es>
2022-05-07 19:06:00 +0100

    Update app

--- Development/app/app.py
+++ Development/app/app.py
@@ -9,46 +9,7 @@ import codecs
 
 def recognition(nameFile):
     # Load the trained model to classify sign
-    path = "/app/traffic-signs-recognition/Development/models/" + nameFile
-    model = load_model(path.encode().decode())
-
-    uploaded_file = st.sidebar.file_uploader("Choose a file", type=["jpg", "png"])
-
-    image = Image.new("RGB", (1, 1))
-    read = False
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        read = True
-
-    if read:
-        read = False
-        click = False
-        _, centerColum, _ = st.columns(3)
-        image = image.resize((30, 30))
-        with centerColum:
-            st.image(image.resize((10 * image.width, 10 * image.height)))
-            if st.button("Classify Image"):
-                click = True
-        if click:
-            image = np.expand_dims(image, axis=0)
-            image = np.array(image)
-            print(image.shape)
-            try:
-                pred = model.predict([image])[0]
-                newPred = pred + 1
-                newPred = [int(i) for i in newPred]
-                maxValue = max(newPred)
-                sign = classes[newPred.index(maxValue) + 1]
-                loadingBar = st.progress(0)
-                for percentComplete in range(100):
-                    time.sleep(0.01)
-                    loadingBar.progress(percentComplete + 1)
-                time.sleep(0.2)
-                st.caption(sign)
-            except ValueError:
-                st.caption(
-                    "The image you have selected cannot be sorted, choose another image."
-                )
+    model = load_model("/app/traffic-signs-recognition/Development/models/" + nameFile)
 
 
 st.title("Traffic Signs Recognition")
@@ -69,7 +30,7 @@ if option != "Home":
                 nameFile = "traffic_classifier.h5"
                 st.caption("The default model is: " + nameFile)
 
-                # recognition(nameFile.encode().decode())
+                recognition(nameFile)
 
             except OSError:
                 st.caption("The default model name does not exist.")
